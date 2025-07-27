bcd2fb581e3afabc6a4bc6ac28b4befa0da445f8
woojin <77970494+woojin966@users.noreply.github.com>
2025-07-27 17:01:16 +0900

    기초

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,27 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build'
+
+  return {
+    plugins: [
     vue(),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+  base: isBuild ? '/vueShoppingMall/' : '/',
+  }
+
+})  
+
