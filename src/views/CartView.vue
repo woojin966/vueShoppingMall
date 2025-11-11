6c5a892f5b65b7e9ea3b4f9f63a48e079c48f79f
woojin <77970494+woojin966@users.noreply.github.com>
2025-11-12 01:36:34 +0900

    장바구니 스타일
    장바구니 기능들 : 전체선택과 선택해제 / 선택한것 구매와 전체구매 / 전체삭제와 선택삭제
    --------
    주문 페이지 스타일
    주문완료 스타일

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,60 +1,111 @@
 <template>
+  <Header />
   <article class="cart_wrap">
     <section class="cart_top_box">
       <h2 class="bb">장바구니</h2>
-      <div class="delete_btn_box">
-        <button
-          @click="
-            () => {
-              clearCart()
-              refreshCart()
-            }
-          "
-        >
-          전체 비우기
-        </button>
-      </div>
     </section>
     <section class="cart_content_box">
-      <ul v-if="cartItems.length">
+      <div class="btn_box top">
+        <div class="sel_btn_box">
+          <button class="all_sel_btn text n">전체선택</button>
+          <button class="sel_off_btn text n">선택해제</button>
+        </div>
+        <!-- <div class="delete_btn_box">
+          <button
+            @click="
+              () => {
+                clearCart()
+                refreshCart()
+              }
+            "
+            class="all_del_btn text n"
+          >
+            전체삭제
+          </button>
+          <button class="sel_del_btn text n">선택삭제</button>
+        </div> -->
+      </div>
+      <ul class="product_list" v-if="cartItems.length">
         <li
           v-for="item in cartItems"
           :key="`${item.id}-${item.option || 'noopt'}`"
           class="cart_item"
         >
-          <img :src="getImgUrl(item.category, item.image)" :alt="item.name" />
+          <input type="checkbox" class="item_check" />
+          <div class="img_box">
+            <img :src="getImgUrl(item.category, item.image)" :alt="item.name" />
+          </div>
 
           <div class="cart_info">
-            <p class="name">{{ item.name }}</p>
-            <p v-if="item.option" class="option">옵션: {{ item.option }}</p>
+            <div class="name_del_row">
+              <div>
+                <p class="name text sb">{{ item.name }}</p>
+                <p v-if="item.option" class="option">옵션: {{ item.option }}</p>
+              </div>
+              <button @click="handleRemove(item.id, item.option)">
+                <span></span>
+                <span></span>
+              </button>
+            </div>
 
             <div class="qty_row">
+              <button class="change_qty_btn minus">
+                <span></span>
+              </button>
               <input
                 type="number"
                 min="1"
                 :value="item.quantity"
                 @change="handleQuantityChange(item.id, item.option, Number($event.target.value))"
+                class="text n"
               />
-              <button @click="handleRemove(item.id, item.option)">삭제</button>
+              <button class="change_qty_btn plus">
+                <span></span>
+                <span></span>
+              </button>
             </div>
 
-            <p class="price">
-              {{ (Number(item.price) * Number(item.quantity)).toLocaleString() }}원
-            </p>
+            <div class="price_row">
+              <p class="price medium sb">
+                {{ (Number(item.price) * Number(item.quantity)).toLocaleString() }}원
+              </p>
+            </div>
           </div>
         </li>
       </ul>
-      <p v-else>장바구니가 비어 있습니다.</p>
+      <p class="empty_cart_msg text sb" v-else>장바구니가 비어 있습니다.</p>
+      <div class="btn_box bottom">
+        <!-- <div class="sel_btn_box">
+          <button class="all_sel_btn text n">전체선택</button>
+          <button class="sel_off_btn text n">선택해제</button>
+        </div> -->
+        <div class="delete_btn_box">
+          <button
+            @click="
+              () => {
+                clearCart()
+                refreshCart()
+              }
+            "
+            class="all_del_btn text n"
+          >
+            전체삭제
+          </button>
+          <button class="sel_del_btn text n">선택삭제</button>
+        </div>
+      </div>
     </section>
     <section class="cart_bottom_box">
       <div class="total_price_box">
-        <p class="total">총합: {{ total.toLocaleString() }}원</p>
+        <p class="total big bb">{{ total.toLocaleString() }}원</p>
       </div>
       <div class="order_btn_box">
-        <button class="order_btn" @click="goToOrder">주문하기</button>
+        <button class="sel_order_btn medium n">선택 주문</button>
+        <button class="all_order_btn medium n" @click="goToOrder">전체 주문</button>
       </div>
     </section>
   </article>
+  <Footer />
 </template>
 
 <script setup>
@@ -62,6 +113,9 @@ import { ref, onMounted } from 'vue'
 import { getCart, removeFromCart, updateQuantity, clearCart, getCartTotal } from '@/api/cart.js'
 import { saveOrder } from '@/api/order.js'
 import { useRouter } from 'vue-router'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
 const router = useRouter()
 
 // 🔗 Vite에서 정적 에셋 동적 경로 만들기
@@ -98,29 +152,6 @@ const goToOrder = () => {
 onMounted(refreshCart)
 </script>
 
-<style scoped>
-.cart_item {
-  display: flex;
-  gap: 12px;
-  align-items: flex-start;
-  margin-bottom: 16px;
-}
-.cart_item img {
-  width: 96px;
-  height: 96px;
-  object-fit: cover;
-}
-.cart_info .name {
-  font-weight: 600;
-}
-.qty_row {
-  display: flex;
-  gap: 8px;
-  align-items: center;
-  margin: 6px 0;
-}
-.total {
-  margin-top: 16px;
-  font-weight: 700;
-}
+<style scoped lang="scss">
+@import '../assets/style/Cart.scss';
 </style>
