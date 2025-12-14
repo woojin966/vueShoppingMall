36543e84f2a84a9b3adbfa3cce3260b8f29a8989
woojin <77970494+woojin966@users.noreply.github.com>
2025-12-14 19:39:53 +0900

    add fallback imgs

--- src/store/randomImages.js
+++ src/store/randomImages.js
@@ -14,14 +14,30 @@ export function randomImages() {
   ]
   const randomFilter = ref('')
 
+  const fallbackImages = ['/fallback/banner1.jpg', '/fallback/banner2.jpg', '/fallback/banner3.jpg']
+
   onMounted(() => {
     const randomIndex = Math.floor(Math.random() * filters.length)
     randomFilter.value = filters[randomIndex]
   })
 
+  // onMounted(async () => {
+  //   const res = await proxy.$unsplash.get('/photos/random')
+  //   imgUrl.value = res.data.urls.regular
+  // })
   onMounted(async () => {
-    const res = await proxy.$unsplash.get('/photos/random')
-    imgUrl.value = res.data.urls.regular
+    // 필터 랜덤
+    randomFilter.value = filters[Math.floor(Math.random() * filters.length)]
+
+    try {
+      const res = await proxy.$unsplash.get('/photos/random')
+      imgUrl.value = res.data.urls.regular
+    } catch (err) {
+      console.warn('🔥 Unsplash 403 → fallback image 사용')
+
+      const pick = Math.floor(Math.random() * fallbackImages.length)
+      imgUrl.value = fallbackImages[pick]
+    }
   })
 
   return { imgUrl, randomFilter }
